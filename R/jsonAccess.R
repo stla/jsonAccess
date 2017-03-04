20f28ea1c61f71afb13331a3606e17c10a48001a
stla <laurent_step@yahoo.fr>
2017-03-04 22:16:09 +0100

    update

--- R/jsonAccess.R
+++ R/jsonAccess.R
@@ -7,46 +7,145 @@
   .C("jsonElemsAtR", json=json, indices=indices,
      lindices=length(indices), result="")$result
 }
+.jsonElemAt <- function(json, index){
+  .C("jsonElemAtR", json=json, index=index, result="")$result
+}
+.jsonElemAt_ifNumber <- function(json, index){
+  .C("jsonElemAt_ifNumberR", json=json, index=index, result="")$result
+}
 
-
-
-
-#' Title
-#'
-#' @param json json string
-#' @param path character vector
-#'
-#' @return json string
+#' @importFrom magrittr %>%
 #' @export
-#' @importFrom jsonlite validate
-#' @importFrom magrittr "%>%"
+magrittr::`%>%`
+
+#' @title Access to the value of a JSON string
+#' @description Access to the value of a JSON string by giving a sequence
+#' of keys.
+#' @param json a JSON string
+#' @param path character vector, sequence of keys
+#' @param toJSON logical, whether to apply \code{\link[jsonlite]{toJSON}} to
+#' the input
+#' @param fromJSON logical, whether to apply \code{\link[jsonlite]{fromJSON}} to
+#' the output
+#'
+#' @return A json string if \code{fromJSON=FALSE}, a R object if \code{fromJSON=TRUE}.
+#' @seealso \code{\link{jsonElemsAt}}
+#' @export
+#' @importFrom jsonlite validate toJSON fromJSON
 #' @examples
 #' json <- "{\"a\":{\"b\":8,\"c\":[1,2]},\"b\":null}"
 #' jsonAccess(json, "a")
 #' jsonAccess(json, "b")
 #' jsonAccess(json, "X")
 #' jsonAccess(json, c("a","c"))
-jsonAccess <- function(json, path){
-  # TODO: check valid json
-  #class(json) <- "json" ...
-  # if(!validate(json))
-  .jsonAccess(json=as.character(json), path=as.character(path))
+#' # get the result as a R object:
+#' jsonAccess(json, c("a","c"), fromJSON=TRUE)
+#' # same as:
+#' jsonAccess(json, c("a","c")) %>% jsonlite::fromJSON
+#' @note Instaed of using the option \code{toJSON=TRUE}, you can apply
+#' \code{\link[jsonlite]{toJSON}} before; this allows you to apply it
+#' with its options. Same remark for the option \code{toJSON=FALSE}.
+jsonAccess <- function(json, path, toJSON=FALSE, fromJSON=FALSE){
+  # check valid json
+  if(toJSON){
+    json <- jsonlite::toJSON(json)
+  }else if(!validate(json)){
+    if(!validate(json <- as.character(json))){
+      stop("Not a valid JSON string. Try option `toJSON=TRUE`.")
+    }
+  }
+  out <- .jsonAccess(json=as.character(json), path=as.character(path))
+  if(fromJSON){
+    out <- jsonlite::fromJSON(out)
+  }
+  return(out)
 }
 
-#' Title
-#'
+#' Sub-array of a JSON array
+#' @description Returns a sub-array of a JSON array by giving the desired
+#' indices.
 #' @param json json string
-#' @param path character vector
-#'
-#' @return json stringjavascript:;
+#' @param indices integer vector
+#' @param toJSON logical, whether to apply \code{\link[jsonlite]{toJSON}} to
+#' the input
+#' @param fromJSON logical, whether to apply \code{\link[jsonlite]{fromJSON}} to
+#' the output
+#' @return A json string if \code{fromJSON=FALSE}, a R object if \code{fromJSON=TRUE}.
 #' @export
-#'
+#' @seealso \code{\link{jsonElemAt}}, \code{\link{jsonAccess}}
 #' @examples
 #' json <- "{\"a\":{\"b\":8,\"c\":[0,1,\"hello\"]},\"b\":null}"
 #' ( arr <- jsonAccess(json, c("a","c")) )
 #' jsonElemsAt(arr, c(0,2))
 #' jsonElemsAt(arr, 2)
 #' jsonElemsAt(arr, c(1,3))
-jsonElemsAt <- function(json, indices){
-  .jsonElemsAt(json=as.character(json), indices=as.integer(indices))
+#' # chain with the pipe operator:
+#' jsonAccess(json, c("a","c")) %>% jsonElemsAt(2)
+jsonElemsAt <- function(json, indices, toJSON=FALSE, fromJSON=FALSE){
+  if(toJSON){
+    json <- jsonlite::toJSON(json)
+  }
+  out <- .jsonElemsAt(json=as.character(json), indices=as.integer(indices))
+  if(fromJSON){
+    out <- jsonlite::fromJSON(out)
+  }
+  return(out)
+}
+
+#' Extract element of a JSON array
+#' @description Returns the element of a JSON array by giving the desired
+#' index.
+#' @param json json string
+#' @param index integer
+#' @param toJSON logical, whether to apply \code{\link[jsonlite]{toJSON}} to
+#' the input
+#' @param fromJSON logical, whether to apply \code{\link[jsonlite]{fromJSON}} to
+#' the output
+#' @return A JSON string if \code{fromJSON=FALSE}, a R object if \code{fromJSON=TRUE}.
+#' @export
+#' @seealso \code{\link{jsonElemsAt}}, \code{\link{jsonElemAt_ifNumber}}, \code{\link{jsonAccess}}
+#' @examples
+#' json <- "[0,1,\"hello\",null,\"bird\"]"
+#' jsonElemAt(json, 2)
+#' jsonElemAt(json, 3)
+#' jsonElemAt(json, 10) == "null"
+#' jsonElemsAt(json, 1:4) %>% jsonElemAt(3)
+#' jsonElemAt("{\"a\":{\"b\":8}}", 0) == "null"
+jsonElemAt <- function(json, index, toJSON=FALSE, fromJSON=FALSE){
+  if(toJSON){
+    json <- jsonlite::toJSON(json)
+  }
+  out <- .jsonElemAt(json=as.character(json), index=as.integer(index))
+  if(fromJSON){
+    out <- jsonlite::fromJSON(out)
+  }
+  return(out)
+}
+
+#' Extract a number from a JSON array
+#' @description Returns the number in a JSON array by giving its index;
+#' returns \code{"null"} if there is no number at this index.
+#' @param json JSON string
+#' @param index integer
+#' @param toJSON logical, whether to apply \code{\link[jsonlite]{toJSON}} to
+#' the input
+#' @param fromJSON logical, whether to apply \code{\link[jsonlite]{fromJSON}} to
+#' the output
+#' @return A JSON string if \code{fromJSON=FALSE}, a R object if \code{fromJSON=TRUE}.
+#' @export
+#' @seealso \code{\link{jsonElemAt}}, \code{\link{jsonAccess}}
+#' @examples
+#' json <- "[1,0,\"hello\",null,\"bird\"]"
+#' jsonElemAt_ifNumber(json, 0)
+#' jsonElemAt_ifNumber(json, 2) == "null"
+#' jsonElemAt_ifNumber(json, 3)
+jsonElemAt_ifNumber <- function(json, index, toJSON=FALSE, fromJSON=FALSE){
+  if(toJSON){
+    json <- jsonlite::toJSON(json)
+  }
+  out <- .jsonElemAt_ifNumber(json=as.character(json), index=as.integer(index))
+  if(fromJSON){
+    out <- jsonlite::fromJSON(out)
+  }
+  return(out)
 }
